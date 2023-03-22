eb0fa27e9d41a1836f2eda3184268178988c14dc
iishchenko <irynaira@gmail.com>
2023-03-22 12:36:17 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,1 @@
-def copy_file(command: str) -> None:
-    if len(command.split()) == 3:
-        command, file_original, file_copy = command.split()
-        if command == "cp" and file_original != file_copy:
-            with (
-                open(file_original, "r") as read,
-                open(file_copy, "w") as write
-            ):
-                write.write(read.read())
+
